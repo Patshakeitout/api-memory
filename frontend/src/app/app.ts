12477cfd64907edc8288ff00e430a9027a6919ae
Patrick <patrick_schauer@t-online.de>
2026-07-12 15:44:05 +0200

    feat: init playerLives

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { OnInit, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  // Grab a couple of things
+  public section = document.querySelector("section");
+  protected playerLives: number = 6;
+
+  ngOnInit(): void {
+    let playerLivesCount = document.querySelector("span");
+    if (playerLivesCount) playerLivesCount.textContent = this.playerLives.toString();
+  }
+
 }
